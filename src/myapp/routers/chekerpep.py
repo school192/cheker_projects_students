292744bc6ee35a11da78cedeab1d06922d403fda
Ivan Voitsik <voitsik.ivan@yandex.ru>
2026-01-21 16:22:55 +0300

    Сделана страница для получения списка ссылок

--- src/myapp/routers/chekerpep.py
+++ src/myapp/routers/chekerpep.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 chekerpep_bp = Blueprint("chekerpep", __name__)
 
 
-@chekerpep_bp.route("/chekerpep")
+@chekerpep_bp.route("/chekerpep", methods=['GET', 'POST'])
 def chekerpep():
-    return render_template("chekerpep.html", title="Проверка на pep8"), 200
+    if request.method == 'GET':
+        return render_template("chekerpep.html", title="Проверка на pep8"), 200
+    print(request.form.get('list_reps'))
+    return ''
